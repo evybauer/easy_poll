8904f9eb9576160561a6e43b9478884f8e8986ed
7 <swistek.labs@gmail.com>
2020-01-12 23:35:02 +0000

    renamed a couple files

--- db/queries/poll_voting_page.sql
+++ db/queries/poll_voting_page.sql
@@ -1,16 +1,15 @@
 -- THIS QUERY IS TO BE USED FOR PULLING DATA TO SHOW ON THE VOTING PAGE OF A SPECIFIC POLL
 
-SELECT options.choice, (SELECT DISTINCT polls.title
+SELECT options.choice, options.description, (SELECT polls.description AS question
   FROM polls
-  WHERE polls.id=1
+  WHERE polls.id=2
   ORDER BY polls.title)
 FROM options
-WHERE poll_id=1;
+WHERE poll_id=2;
 
-
-
-
-
+-- SELECT *
+-- FROM options
+-- WHERE poll
 
 
 -- SELECT DISTINCT polls.title, (SELECT options.choice
@@ -24,4 +23,3 @@ WHERE poll_id=1;
 -- SELECT options.choice
 -- FROM options
 -- WHERE poll_id=1;
-
