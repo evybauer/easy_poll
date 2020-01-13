843fc2cada458299403499362fa95a78169d0d4c
7 <swistek.labs@gmail.com>
2020-01-13 22:55:12 +0000

    query for login, and query for individual poll page

--- routes/polls.js
+++ routes/polls.js
@@ -1,17 +1,24 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-module.exports = (db) => {
+module.exports = db => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
+    db.query(
+      `
+    SELECT options.choice, options.description, (SELECT polls.description AS question
+      FROM polls
+      WHERE polls.id=2
+      ORDER BY polls.title)
+    FROM options
+    WHERE poll_id=2;
+    `
+    )
       .then(data => {
         const polls = data.rows;
         res.json({ polls });
       })
       .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+        res.status(500).json({ error: err.message });
       });
   });
   return router;
@@ -26,8 +33,6 @@ module.exports = (db) => {
 //     res.redirect("/");
 // });
 
-
-
 // SEE POLLS CREATED
 
 // CREATE THE POLL
@@ -39,23 +44,21 @@ module.exports = (db) => {
 
 router.post("/", (req, res) => {
   //const userId = req.session.userId;
-  database.addPoll({...req.body})
-  .then(poll => {
-    res.send(poll);
-  })
-  .catch(e => {
-    console.error(e);
-    res.send(e)
-  });
+  database
+    .addPoll({ ...req.body })
+    .then(poll => {
+      res.send(poll);
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    });
 });
-
 
 //SEE THE POLL
 //Where votes happen
 
 // SEE THE RESULTS
-
-
 
 // The creator will be loged in and will be able to see previous polls
 // Click on create and be redirected to the /polls/new where will fill out a new poll
