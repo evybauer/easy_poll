e515333328263668f41f1e9e6b5718d3aa085216
7 <swistek.labs@gmail.com>
2020-01-13 21:29:16 +0000

    changed the options and votes tables, removed rating row for starts, dont need for our voting system

--- db/queries/votes_for_poll.sql
+++ db/queries/votes_for_poll.sql
@@ -1,7 +1,12 @@
 -- This query is to pull up data regarding the votes submitted for a certain poll
 
 
-SELECT DISTINCT polls.title, polls.description, options.choice, options.description AS genre, votes.rating AS user_question_feedback
+-- SELECT DISTINCT polls.title, polls.description, options.choice, options.description AS genre, votes.rating AS user_question_feedback
+-- FROM polls, options
+-- JOIN votes ON options_id=options.id
+-- WHERE polls.id=2 AND options.poll_id=2;
+
+SELECT DISTINCT polls.title, polls.description, options.choice, options.description AS genre, options.vote_total AS votes
 FROM polls, options
-JOIN votes ON options_id=options.id
+-- JOIN votes ON options_id=options.id
 WHERE polls.id=2 AND options.poll_id=2;
