cd058988e0b74fee38f1c01e292afa6b09e33e5a
7 <swistek.labs@gmail.com>
2020-01-13 23:27:20 +0000

    finished the query to see any specfific poll, not dynamic yet, hard coded

--- routes/polls.js
+++ routes/polls.js
@@ -1,29 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-module.exports = db => {
-  router.get("/", (req, res) => {
-    db.query(
-      `
-    SELECT options.choice, options.description, (SELECT polls.description AS question
-      FROM polls
-      WHERE polls.id=2
-      ORDER BY polls.title)
-    FROM options
-    WHERE poll_id=2;
-    `
-    )
-      .then(data => {
-        const polls = data.rows;
-        res.json({ polls });
-      })
-      .catch(err => {
-        res.status(500).json({ error: err.message });
-      });
-  });
-  return router;
-};
-
 // router.post("/", (req, res) => {
 //   pollsDatabase = {
 //     title: req.body. //
@@ -35,28 +12,64 @@ module.exports = db => {
 
 // SEE POLLS CREATED
 
-// CREATE THE POLL
+module.exports = db => {
+  router.get("/user1", (req, res) => {
+    let query = `SELECT * FROM polls WHERE creator_id=1`;
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const polls = data.rows;
+        res.json({ polls });
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
-// router.get("/polls", (req, res) => {
-//   db.query(...)
-//   authentication of the user
-// })
+  // CREATE THE POLL
 
-router.post("/", (req, res) => {
-  //const userId = req.session.userId;
-  database
-    .addPoll({ ...req.body })
-    .then(poll => {
-      res.send(poll);
-    })
-    .catch(e => {
-      console.error(e);
-      res.send(e);
-    });
-});
+  // router.get("/polls", (req, res) => {
+  //     db.query(...)
+  //     // authentication of the user
+  // });
 
-//SEE THE POLL
-//Where votes happen
+  router.post("/", (req, res) => {
+    //const userId = req.session.userId;
+    database
+      .addPoll({ ...req.body })
+      .then(poll => {
+        res.send(poll);
+      })
+      .catch(e => {
+        console.error(e);
+        res.send(e);
+      });
+  });
+
+  //SEE THE POLL
+  //Where votes happen
+  router.get("/", (req, res) => {
+    db.query(
+      `
+          SELECT options.choice, options.description, (SELECT polls.description AS question
+            FROM polls
+            WHERE polls.id=2
+            ORDER BY polls.title)
+          FROM options
+          WHERE poll_id=2;
+          `
+    )
+      .then(data => {
+        const polls = data.rows;
+        console.log(polls);
+        res.json({ polls });
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  return router;
+};
 
 // SEE THE RESULTS
 
