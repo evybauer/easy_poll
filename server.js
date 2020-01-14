2b2a9a833a82e87710c5840c88f1e06e573512e4
Evelyn Bauer <evybauer@gmail.com>
2020-01-14 03:54:09 +0000

    Change router redirection

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use("/api/polls", pollsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("login");
 });
 
 app.listen(PORT, () => {
